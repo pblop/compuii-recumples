3088f82dcb83df66ff7fdf12c19b029df12b6f98
Pablo Pérez <pablo@pabl.eu>
2022-03-16 22:38:14 +0100

    Plantilla.

---
+++ trabajo.asm
@@ -0,0 +1,25 @@
+.area PROG (ABS)
+
+; CONSTANTES
+fin      .equ 0xFF01
+pantalla .equ 0xFF00
+teclado  .equ 0xFF02
+
+; DIRECTIVAS
+.org 0x100
+.globl programa
+
+; VARIABLES
+ano:      .word 0x1969
+mes:      .byte 0x7
+dia:      .byte 0x27
+nCumples: .byte 10
+
+programa:
+  ; Final del programa
+  clra
+  sta fin
+
+; Iniciar el programa en 'programa'
+.org 0xFFFE
+.word programa
