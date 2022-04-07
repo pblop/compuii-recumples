37ca072e9bc4b2fee56d72c421642d6404fe2d59
Pablo Pérez <pablo@pabl.eu>
2022-04-07 16:10:40 +0200

    Ignorar *.lst y imprimir mes

--- meses.asm
+++ meses.asm
@@ -9,46 +9,82 @@ teclado  .equ 0xFF02
 .org 0x100
 .globl programa
 
-enero:  .ascii "enero"
-        .byte 0
+; VARIABLES
 
-febrero:  .ascii "febrero"
-        .byte 0
+mes: .byte 0x13
 
-marzo:  .ascii "marzo"
-        .byte 0
-
-abril:  .ascii "abril"
-        .byte 0
-
-mayo:  .ascii "mayo"
-        .byte 0
-
-junio:  .ascii "junio"
-        .byte 0
-
-julio:  .ascii "julio"
-        .byte 0
-
-agosto:  .ascii "agosto"
-        .byte 0
-
+enero:       .ascii "enero"
+             .byte 0
+febrero:     .ascii "febrero"
+             .byte 0
+marzo:       .ascii "marzo"
+             .byte 0
+abril:       .ascii "abril"
+             .byte 0
+mayo:        .ascii "mayo"
+             .byte 0
+junio:       .ascii "junio"
+             .byte 0
+julio:       .ascii "julio"
+             .byte 0
+agosto:      .ascii "agosto"
+             .byte 0
 septiembre:  .ascii "septiembre"
-        .byte 0
+             .byte 0
+octubre:     .ascii "octubre"
+             .byte 0
+noviembre:   .ascii "noviembre"
+             .byte 0
+diciembre:   .ascii "diciembre"
+             .byte 0
+tablames:
+  .word enero
+  .word febrero
+  .word marzo
+  .word abril
+  .word mayo
+  .word junio
+  .word julio
+  .word agosto
+  .word septiembre ; 0x9
+  .word octubre    ; 0x10
+  .word noviembre  ; 0x11
+  .word diciembre  ; 0x12
 
-octubre:  .ascii "octubre"
-        .byte 0
-
-noviembre:  .ascii "noviembre"
-        .byte 0
-
-diciembre:  .ascii "diciembre"
-        .byte 0
 
 programa:
+  ldx #tablames
+  ; 123 ciclos
+  ldb mes
+  cmpb #0x10
+  blt menor10
+  subb #(0x10-0x9) ; Si el número es >=0x10, le restamos 0x6 para que
+                   ; continúe en el
+
+  menor10:
+    decb
+    aslb
+    ldx b,x
+
+  ; 123 ciclos
+  ;lda mes
+  ;deca
+  ;asla
+  ;ldx #tablames
+  ;leax a,x
+  ;ldx ,x
+
+  bucle:
+    lda ,x+
+    beq acabar
+    sta pantalla
+    bra bucle
+
+
   ; Final del programa
-  clra
-  sta fin
+  acabar:
+    clra
+    sta fin
 
 ; Iniciar el programa en 'programa'
 .org 0xFFFE
