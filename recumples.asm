b80428f95cb9f2565b6c49c25df22b936fa35867
pablo <pablo@pabl.eu>
2022-05-15 20:57:06 +0200

    antes de cambiar 0,u por 1,u y viceversa

--- recumples.asm
+++ recumples.asm
@@ -59,33 +59,6 @@ tabladiasmes:
   .byte 0x31
 
 ; Función.
-; Actualiza el valor de la tabla de los días de los meses
-; en función de si el año actual es bisiesto.
-; Entrada: nada
-; Salida: los días de febrero en la tabladiasmes
-; Afecta: D, tabladiasmes
-actualiza_bisiesto:
-  ldd ano ;; TODO: Posiblemente optimizable (a lo mejor haciendo un ldb con sólo el último byte 
-          ;; de ano).
-
-  ; Si el último bit de la última cifra es 1, no es bisiesto.
-  bitb #0b00000001
-  bne ab_no_bisiesto
-
-  andb #0b00010010
-  beq ab_si_bisiesto
-  cmpb #0b00010010
-  beq ab_si_bisiesto
-
-  ab_no_bisiesto:
-    ldb #0x28
-  ab_si_bisiesto:
-    ldb #0x29
-  ab_ret:
-    stb tabladiasmes+2-1
-    rts
-
-; Función.
 ; Imprime la cadena ASCII marcada por X por pantalla.
 ; Entrada: X (dirección de la cadena a imprimir)
 ; Salida: pantalla
@@ -119,7 +92,7 @@ imprimeMes:
     aslb
     ldx b,x
 
-  jsr imprimeASCII
+  bsr imprimeASCII
   rts
 
 
@@ -173,7 +146,8 @@ imprimeBCD:
 ; Afecta: X
 imprimeDe:
   ldx #de
-  jsr imprimeASCII
+    pulu x, d
+  bsr imprimeASCII
 
   rts
 
@@ -185,30 +159,128 @@ imprimeDe:
 ;          B (día (BCD))
 ; Salida: pantalla
 ; Afecta: X, D
-imprimeFecha:
+imprime_fecha:
   ldb #0
-  jsr imprimeBCD
-  jsr imprimeDe
-  jsr imprimeMes
-  jsr imprimeDe
+  bsr imprimeBCD
+  bsr imprimeDe
+  bsr imprimeMes
+  bsr imprimeDe
   ldb #1
-  jsr imprimeBCD
+  bsr imprimeBCD
 
   rts
 
+; Función: inc8.
+;   Incrementa un número de 8 bits
+; Entrada: a (número a incrementar)
+; Salida: a
+; Registros afectados: b
+inc8:
+  inca
+  tfr a,b
+  andb #0x0f
+  cmpb #0x0a
+  bne inc8_segunda
+  adda #6
+inc8_segunda:
+  cmpa #0xa0
+  blo inc8_ret
+  clra
+inc8_ret:
+  rts
+
 ; Función.
-; Corrige los meses para que sean válidos (no sean >12), ajustando los años
-; a la vez.
-; Entrada: stack u.
-; Salida:  stack u.
+;   Incrementa un año (16 bits).
+; Entrada: año (stack)
+; Salida: año (stack)
+; Registros afectados: a
+incano:
+  lda 3,u
+  bsr inc8
+  sta 3,u
+  cmpa #0
+  beq incano_2000
+  ; No debería de hacer falta esta línea.
+  ; lda #0x19
+  rts
+incano_2000:
+  lda #0x20
+  sta 2,u
+  rts
+
+
+; Función.
+;   Vuelve a enero cuando estamos en el mes 13 e incrementa el año.
+; Entrada: b (mes) 
+; Salida: b (mes modificado)
 ; Afecta: 
-corregirMes:
-  rts
+corregir_mes:
+  cmpb #0x12
+  bhi cm_cuerpowhile
+
+  rts 
+
+  cm_cuerpowhile:
+    ;; cuerpo del while
+    subb #0x12
+    bsr incano
+  
+    bra corregir_mes
 
 ; Función.
-;
-corregirDia:
-  rts
+; Actualiza el valor de la tabla de los días de los meses
+; en función de si el año actual es bisiesto.
+; Entrada: nada
+; Salida: los días de febrero en la tabladiasmes
+; Afecta: D, tabladiasmes
+actualiza_bisiesto:
+  ldd ano ;; TODO: Posiblemente optimizable (a lo mejor haciendo un ldb con sólo el último byte 
+          ;; de ano).
+
+  ; Si el último bit de la última cifra es 1, no es bisiesto.
+  bitb #0b00000001
+  bne ab_no_bisiesto
+
+  andb #0b00010010
+  beq ab_si_bisiesto
+  cmpb #0b00010010
+  beq ab_si_bisiesto
+
+  ab_no_bisiesto:
+    ldb #0x28
+  ab_si_bisiesto:
+    ldb #0x29
+  ab_ret:
+    stb tabladiasmes+2-1
+    rts
+
+; Funcion.
+; Corregir el dia si nos pasamos de los que puede tener un mes
+; Entrada: a (dia)
+;          b (mes)
+; Salida:  a (dia)
+; Registros afectados: a, b
+corregir_dia:
+  ldx tabladiasmes-1
+  cd_while:
+    bsr actualiza_bisiesto
+
+    cmpa b, x ; numero de dias del mes en el que estamos
+    ble cd_ret
+
+
+    suba b, x ; dia -= dias[mes-1]
+
+    exg a,b   ; 
+    bsr inc8  ; mes++
+    exg b,a   ;
+    
+    bsr corregir_mes
+
+    bra cd_while
+
+  cd_ret:
+    rts
 
 programa:
   ; Inicializar stacks.
@@ -216,12 +288,12 @@ programa:
   ldu #0xE000
   ; STACK U    (5, u)
   ; 1: i       (4, u)
-  ; 2: ano     (2, u)
-  ; 1: mes     (1, u)
-  ; 1: dia     (0, u)
+  ; 2: ano     (2, u) => 19: 2,u. 69: 3,u.
+  ; 1: dia     (1, u)
+  ; 1: mes     (0, u)
 
   ; Bucle para i.
-  lda #1
+  lda #0
   pshu a     ; i = 0
   mbuclei:
     ; Cargar dia, mes y año en el stack.
@@ -229,21 +301,25 @@ programa:
     ldd mes     ; Cargo mes y día en d (de tal forma que quedan como en el
                 ; esquema de arriba.
     pshu x, d
-
-    inc16 2,u  ; año += 1
-    inc8 1,u ; mes += 1
     
-    bsr corregirMes
-    bsr corregirDia
+    bsr incano  ; año += 1
+    lda 1, u
+    bsr inc8    ; mes += 1
+    sta 1, u
+    
+    bsr corregir_mes
+    bsr corregir_dia
 
-    inc8 ,u
+    lda 0, u
+    bsr inc8
+    sta 0, u
 
-    bsr corregirDia
+    bsr corregir_dia
 
-    bsr imprimeFecha
+    lbsr imprime_fecha
 
-    ; Hacer pulu de x y d, o hacer x+=3
-    leau 3,x
+    ; Hacer pulu de x y d, o hacer u+=3
+    leau 3,u
 
     inc 4,u
     lda 4,u
@@ -257,6 +333,5 @@ programa:
 ; Iniciar el programa en 'programa'
 .org 0xFFFE
 .word programa
-
 
 ;; TE QUIERO <3
